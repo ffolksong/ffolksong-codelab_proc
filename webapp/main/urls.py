e1166e8d4fc8c15bd0cff3726758c588dbd7eede
ffolksong <nattawutb44@gmail.com>
2022-10-03 22:46:50 +0700

    firstsecondcommit

---
+++ webapp/main/urls.py
@@ -0,0 +1,14 @@
+#urls.py
+
+from http import client
+from django.urls import path
+from .views import (
+    list_view,
+    client #new function in  views.py    
+)
+
+app_name = 'main'
+urlpatterns = [
+    path('', list_view, name='home-list'),
+    path('join-now',client, name='hangout'),#new url route
+]
